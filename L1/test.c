e872f00e9e29f750871e37dedafe2ce8c0e578eb
jauthier <jauthier@mail.uoguelph.ca>
2016-05-16 13:32:45 -0400

    go

---
+++ L1/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "fractions.h"
+
+int main(int argc, char * argv[]){
+    
+    int a, b, c, d, check;
+    char temp[100];
+    printf("Welcome to fraction test program\n");
+    check = 0;
+    while (check == 0){
+        printf("Enter a numerator:");
+        scanf("%d", temp);
+        a = atol(temp);
+        check = 1;
+    }
+    
+    
+    
+    
+}
