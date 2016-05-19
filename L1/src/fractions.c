1e4c2e75633de5b2c7397a108653397ecf15c953
jauthier <jauthier@mail.uoguelph.ca>
2016-05-19 13:05:47 -0400

    go

--- L1/src/fractions.c
+++ L1/src/fractions.c
@@ -1,5 +1,26 @@
+/*
+Fraction ADT
+
+Author: Jessica Authier, 0849720
+
+This file contains functions that create, manipulate and destroy 
+fraction data types.
+
+*/
+
 #include "fractions.h"
 
+/*
+    createFraction takes two intergers, one for the numerator and one for the denominator,
+    and creates a fraction with them. 
+    
+    Parameters: int numer --> the numerator of the fraction
+                int denom --> the denominatorof the fraction
+    
+    Return: if the fraction is valid --> pointer to the fraction
+            if the fraction isn't valid (dividing by 0) --> NULL
+        
+*/
 Fraction *createFraction( int numer, int denom) {
     
     if (denom != 0){
@@ -10,21 +31,46 @@ Fraction *createFraction( int numer, int denom) {
         return frac;
     }
     else {
-        printf("You cannot divide by 0!\nYour fraction was not created\n");
+        //printf("You cannot divide by 0!\nYour fraction was not created\n");
         return NULL;
     }
 }
 
+/*
+    getNumerator takes a fraction and returns its numerator
+    
+    parameter: frac --> pointer to a fraction
+
+    return: the numerator of the given fraction
+    
+*/
 int getNumerator(Fraction *frac) {
     
     return frac->numerator;
 }
 
+/*
+    getDenominator takes a fraction and returns its denominator
+    
+    parameter: frac --> pointer to a fraction
+
+    return: the denominator of the given fraction
+    
+*/
 int getDenominator(Fraction *frac) {
     
     return frac->denominator;
 }
 
+/*
+    add takes two fractions and adds them, keeping them in their fraction form
+    
+    parameters: f1 --> pointer to the first fraction
+                f2 --> pointer to the second fraction
+                
+    return: a pointer to the sum fraction
+    
+*/
 Fraction *add(Fraction *f1, Fraction *f2) {
     
     Fraction *sum;
@@ -48,6 +94,15 @@ Fraction *add(Fraction *f1, Fraction *f2) {
     return sum;
 }
 
+/*
+    multiply takes two fractions and multiplies them, keeping them in their fraction form
+    
+    parameters: f1 --> pointer to the first fraction
+                f2 --> pointer to the second fraction
+                
+    return: a pointer to the product fraction
+    
+*/
 Fraction *multiply (Fraction *f1, Fraction *f2) {
     
     Fraction *product;
@@ -67,6 +122,14 @@ Fraction *multiply (Fraction *f1, Fraction *f2) {
     return product;
 }
 
+/*
+    destroyFraction gets rid of the fraction and frees the memory is was occuping
+    
+    parameter: toDestroy --> a pointer to the fraction you wish to destroy
+    
+    return: none 
+
+*/
 void destroyFraction(Fraction *toDestroy) {
     
     free(toDestroy);
--- L1/src/test.c
+++ L1/src/test.c
@@ -5,32 +5,49 @@ int main(int argc, char * argv[]){
     
     int a, b, c, d;
     Fraction *fract, *fract2, *sum, *product;
-
+    int check = 0;
+    
     printf("Welcome to fraction test program\n");
     
     printf("Enter a numerator:\n");
     scanf("%d", &a);
 
-    printf("Enter a denominator:\n");
-    scanf("%d", &b);
+    while (check == 0){
+        printf("Enter a denominator:\n");
+        scanf("%d", &b);
+        
+        fract = createFraction(a, b);
+        if (fract != NULL) /*Check if the denominator is 0, if it is the user is prompted to enter a new number*/
+            check = 1;
+        else 
+            printf("You cannot divide by 0!\n");
+    }
     
-    fract = createFraction(a, b);
-    if ()//check if returns null
-
     printf("The numerator is: %d, the denominator is: %d.\n", getNumerator(fract), getDenominator(fract));
     
+    check = 0;
     printf("Lets make another fraction.\nEnter a numerator:\n");
     scanf("%d", &c);
 
-    printf("Enter a denominator:\n");
-    scanf("%d", &d);
-
-    fract2 = createFraction(c, d);
+    while (check == 0){
+        printf("Enter a denominator:\n");
+        scanf("%d", &d);
+        
+        fract = createFraction(c, d);
+        if (fract != NULL) /*Check if the denominator is 0, if it is the user is prompted to enter a new number*/
+            check = 1;
+        else 
+            printf("You cannot divide by 0!\n");
+    }
+    
     printf("The numerator is: %d, the denominator is: %d.\n", getNumerator(fract2), getDenominator(fract2));
 
+    
+    /*Test add function*/
     sum = add(fract, fract2);
     printf("The sum is: %d/%d.\n", getNumerator(sum), getDenominator(sum));
 
+    /*Test multiply function*/
     product = multiply(fract, fract2);
     printf("The product is: %d/%d.\n", getNumerator(product), getDenominator(product));
 
