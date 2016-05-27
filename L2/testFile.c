8fde7a52d19f53c51f72a165ce1db7281ebcb16f
jauthier <jauthier@mail.uoguelph.ca>
2016-05-27 11:38:33 -0400

    go

--- L2/testFile.c
+++ L2/testFile.c
@@ -3,9 +3,25 @@
 int main(int argc, char *argv[]){
     
     List * testList;
-    testList = createList();
+    testList = createList(); //test createList
     
-    int first;
+    int first, second, third, len;
+    Element *e1, *e2, *e3;
+    
+    first = 1;
+    second = 2;
+    third = 3;
+    
+    e1 = createElement(&first);
+    e2 = createElement(&second);
+    e3 = createElement(&third);
+    
+    testList = addFront(e1, testList);
+    len = getLength(testList);
+    printf("the list has %d element(s)\n", len);
+    Element * temp = getFront();
+    printf("The first element: %d\n", *(int *)temp->elementPtr);
+    
     
     
     return 0;
