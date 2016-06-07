100725e3bdf5187a9ba8c7e786553743385fcc88
jauthier <jauthier@mail.uoguelph.ca>
2016-06-07 10:11:26 -0400

    go

---
+++ A1/maze.c
@@ -0,0 +1,85 @@
+
+#include "maze.h"
+
+Square * current;
+Square * start;
+Square * finish;
+
+
+
+int main(int argc, char * argv[]){
+    
+    char * fileName;
+    
+    if (argc != 2){
+        printf("Incorrect input\n");
+        exit(0);
+    }
+    
+    fileName = argv[1];
+    
+    parseFile(fileName);
+    
+    
+    return 0;
+}
+
+
+void parseFile(char * fileName){
+    
+    FILE * fp;
+    char temp;
+    int xCount;
+    int yCount;
+    Square * hold;
+    Square * grid[100][100];
+    
+    fp = fopen(fileName, "r");
+    yCount = 0;
+    temp = fgetc(fp);
+    
+    while (temp != EOF){ // while we are not at the end of the file
+        xCount = 0;
+        while(temp != "\n"){ //while we are not at the end of the line
+            
+            hold = createSquare(temp, xCount, yCount);
+            grid[xCount][yCount] = hold;
+            
+            if (temp == 'S'){
+                start = hold;
+                current = hold;
+            } else if (temp == 'F')
+                finish = hold;
+            
+            temp = fgetc(fp);
+            xCount++; // increment in the x direction
+        }
+        yCount++;
+    }
+    
+}
+
+
+Square * createSquare(char v, int x, int y){
+    
+    Square * newSquare;
+    newSquare = malloc(sizeof(Square));
+    
+    newSquare->val = v;
+    
+    if (v == '|' || v == '+' || v == '-')
+        newSquare->wall = 1;
+    else
+        newSquare->wall = 0;
+    
+    newSquare->x = x;
+    newSquare->y = y;
+    
+    if (v == 'S')
+        newSquare->wasHere = 1;
+    else
+        newSquare->wasHere = 0;
+    
+    return newSquare;
+}
+
---
+++ A1/maze.h
@@ -0,0 +1,24 @@
+
+
+
+//need a matrix of size n x m
+
+typedef struct Square{
+    
+    char val; // the char the square contains
+    int wall; // is this square a wall
+    int x; // x coordinate
+    int y; // y coordinate
+    int wasHere; //have we been here before
+    
+} Square;
+
+
+/*
+    either use left had or right hand rule
+    only turning left of right.  
+    char dir; //direction, n = north, s = south, e = east, w = west
+
+*/
+
+
