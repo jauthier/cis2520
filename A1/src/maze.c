62c6303de25465726d33796a170dad6b51a010cc
Jessica <jauthier95@gmail.com>
2016-06-10 11:35:50 -0400

    blah

--- A1/src/maze.c
+++ A1/src/maze.c
@@ -10,6 +10,8 @@ int sizeX;
 int sizeY;
 char dir;
 
+void stackThing();
+
 int main(int argc, char * argv[]){
     
     char * fileName;
@@ -27,7 +29,11 @@ int main(int argc, char * argv[]){
     printMaze();
     
     solution = createStack();
-    
+    printf("Stack created\n");
+    navigate();
+    stackThing();
+    printMaze();
+
     return 0;
 }
 
@@ -129,6 +135,7 @@ int checkUp(){
     int x, y;
     x = current->x;
     y = current->y;
+    printf("in checkUp\n");
     
     if (maze[x][y-1]->wall == 0){
         return 1;
@@ -141,6 +148,7 @@ int checkDown(){
     int x, y;
     x = current->x;
     y = current->y;
+    printf("in checkDown\n");
     
     if (maze[x][y+1]->wall == 0){
         return 1;
@@ -153,7 +161,8 @@ int checkRight(){
     int x, y;
     x = current->x;
     y = current->y;
-    
+    printf("in checkRight\n");
+
     if (maze[x+1][y]->wall == 0){
         return 1;
     }
@@ -165,8 +174,10 @@ int checkLeft(){
     int x, y;
     x = current->x;
     y = current->y;
+    printf("in checkLeft\n");
     
     if (maze[x-1][y]->wall == 0){
+        printf("no wall\n");
         return 1;
     }
     return 0;
@@ -176,76 +187,123 @@ void changeDir(){
     
     switch (dir){
         case 'n':
-           dir = 'e'; 
+           dir = 'e';
+           break;
         case 's':
             dir  = 'w';
+            break;
         case 'e':
             dir = 's';
+            break;
         case 'w':
             dir = 'n';
+            break;
     }
- }
+}
+
+void stackThing(){
+
+    Element * hold;
+    hold = solution->top->head;
+    
+    while (hold != NULL){
+        Square * temp = (Square *)hold->elementPtr;
+        temp->val = '.';
+        hold = hold->next;
+        
+    }
+}
 
 void navigate(){
     
     // this is where the maze will be solved
     //start by getting the initial direction
-    int check, nextX, nextY;
+    int check, nextX, nextY, lastX, lastY;
     initDir();
-    
     //add the staeting square to the spot
     push(solution, current);
+    printf("first square added\n");
     
     while (current->val != 'F'){
-        
+        printf("in loop: (%d, %d)\n",current->x, current->y);
+
         //can i go forward?
         switch(dir){
             case 'n':
                 check = checkUp();
-                nextX = current->x - 1;
-                nextY = current->y;
-            case 's':
-                check = checkDown();
-                nextX = current->x + 1;
-                nextY = current->y;
-            case 'e':
-                check = checkRight();
-                nextX = current->x;
-                nextY = current->y + 1;
-            case 'w':
-                check = checkLeft(); 
                 nextX = current->x;
                 nextY = current->y - 1;
+                break;
+            case 's':
+                check = checkDown();
+                nextX = current->x;
+                nextY = current->y + 1;
+                break;
+            case 'e':
+                check = checkRight();
+                nextX = current->x + 1;
+                nextY = current->y;
+                break;
+            case 'w':
+                check = checkLeft(); 
+                nextX = current->x - 1;
+                nextY = current->y;
+                break;
         }
+        printf("switch return: %d\n",check);
+        char c = getchar();
         
-        if (check == 1){ // if you can move, the next space is not a wall 
+        if (check == 1){ // if you can move, the next space is not a wall
+            printf("wasHere: %d\n",maze[nextX][nextY]->wasHere);
             if (maze[nextX][nextY]->wasHere == 1){ //we have been there already
-                if (otherOptions(nextX, nextY) == 1){ // there are other options
+                printf("last: (%d, %d)\n",lastX,lastY);
+                if (otherOptions(lastX, lastY) == 1){ // there are other options
                     changeDir();
                 } else { // no other options will need to go back to where we have been
                      pop(solution); // pop from stack
+        lastX = current->x;
+        lastY = current->y;
+        printf("(%d, %d)\n", lastX, lastY);
                     current = maze[nextX][nextY]; // move
                 }
             } else {
+        lastX = current->x;
+        lastY = current->y;
+        printf("(%d, %d)\n", lastX, lastY);
                 current = maze[nextX][nextY]; // move
                 current-> wasHere = 1;
-                push(solution, current); // add to stack                
+                push(solution, current); // add to stack
             }
         } else {
+            printf("changing dir. before: %c\n",dir);
             changeDir();
+            printf("after: %c\n",dir);
         }
     }
-    
-    
 }
 
-int otherOptions(int x1, int y1){
+int otherOptions(int lastX, int lastY){
     //check if there are other open squares next to the current
-    int x, y; 
+    int x, y, up, down, left, right;
     x = current->x;
     y = current->y;
-    
-    if ((maze[x-1][y]->wasHere == 0)||(maze[x+1][y]->wasHere == 0)||(maze[x][y-1]->wasHere == 0)||(maze[x][y+1]->wasHere == 0))
+
+    printf("%d, %d\n",lastX, lastY);
+
+    up = maze[x][y-1]->wasHere;
+    if ((maze[x][y-1]->wall == 1)||(x == lastX && y-1 == lastY))
+        up = 1;
+    down = maze[x][y+1]->wasHere;
+    if ((maze[x][y+1]->wall == 1)||(x == lastX && y+1 == lastY))
+        down = 1;
+    right = maze[x+1][y]->wasHere;
+    if ((maze[x+1][y]->wall == 1)||(x+1 == lastX && y == lastY))
+        right = 1;
+    left = maze[x-1][y]->wasHere;
+    if ((maze[x-1][y]->wall == 1)||(x-1 == lastX && y == lastY))
+        left = 1;
+
+    if ((up == 0)||(down == 0)||(left == 0)||(right == 0))
         return 1;
     else 
         return 0;
