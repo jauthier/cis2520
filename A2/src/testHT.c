98b83df0f477186a0e7133936384186409f0f296
jauthier <jauthier@mail.uoguelph.ca>
2016-07-08 13:17:04 -0400

    A2

--- A2/src/testHT.c
+++ A2/src/testHT.c
@@ -7,7 +7,7 @@ int main(){
     int check;
     int value1, value2;
     Entry ** ht = create();
-    long key1, key2, key3, key4;
+    long key1, key2;
     
     key1 = 5199961212;
     value1 = 11;
