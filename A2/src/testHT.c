4c925475a247967ed0e28cdef734bdfe1cd823c8
jauthier <jauthier@mail.uoguelph.ca>
2016-07-08 12:58:34 -0400

    A2

--- A2/src/testHT.c
+++ A2/src/testHT.c
@@ -7,8 +7,9 @@ int main(){
     int check;
     int value1, value2, value3, value4;
     Entry ** ht = create();
-    long key1 = 5199961212;
+    long key1, key2, key3, key4;
     
+    key1 = 5199961212;
     value1 = 11;
     insert(ht, key1, &value1);
     check = exists(ht, key1);
@@ -19,10 +20,14 @@ int main(){
     
     check = *(int *)lookup(ht, key1);
     
-    printf("the value shouldbe: %d, the value is: %d", value1, check);
-    
-    
+    printf("the value should be: %d, the value is: %d\n", value1, check);
     //if exists returns 1 and lookup returns the value then we know insert worked
+    
+    key2 = 5199961212; // same key to test if insert can handle it
+    value2 = 12;
+    insert(ht, key2, value2);
+    
+    
     return 0;
 }
 
