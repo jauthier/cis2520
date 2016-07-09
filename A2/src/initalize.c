39d3fe85536dcfd2da3d66f7ea68099a4efbef24
Jessica <jauthier95@gmail.com>
2016-07-09 11:34:09 -0400

    go

--- A2/src/initalize.c
+++ A2/src/initalize.c
@@ -24,23 +24,21 @@ List * loadFile(char * fileName, List *list){
     //fgets(buffer, 200, fp);
     
     
-    while (*fgets(buffer, 200, fp) != EOF){
-        printf("%s\n",buffer);
+    while (fgets(buffer, 200, fp) != NULL){
         lastName = strtok(buffer, ",");
-        printf("%s\n",lastName);
         firstName = strtok(NULL, ",");
-        printf("%s\n",firstName);
         phoneNum = strtok(NULL, ", \n");
         
         Person * newPerson = malloc(sizeof(Person));
-        newPerson->lastName = lastName;
-        newPerson->firstName = firstName;
+        newPerson->lastName = malloc(sizeof(char)*strlen(lastName));
+        strcpy(newPerson->lastName, lastName);
+        newPerson->firstName = malloc(sizeof(char)*strlen(firstName));;
+        strcpy(newPerson->firstName, firstName);
 
         long hold = strtol((char*)phoneNum, &temp, 10);
 
         newPerson->phoneNum = hold;
-        printf("%ld\n", newPerson->phoneNum);
-        
+        printf("%s, %s, %ld\n",newPerson->lastName, newPerson->firstName, newPerson->phoneNum);
         Element * newElement = createElement(newPerson);
         
         //add to a list of people
@@ -61,14 +59,20 @@ List * mergeSort(List * list){
     
     if (getLength(list)<2)
         return list;
-    
     list1->head = head;
+    //printf("list1: %s\n", ((Person*)(list1->head->elementPtr))->lastName);
     list2->head = bisectList(head)->next;
+    //printf("list2: %s\n", ((Person*)(list2->head->elementPtr))->lastName);
     bisectList(head)->next = NULL;
-    
+
+    printf("\nlist1: ");
+    printList(list1);
+    printf("list2: ");
+    printList(list2);
+
     list1 = mergeSort(list1);
     list2 = mergeSort(list2);
-    
+printf("going to merge\n");
     return mergeLists(list1->head, list2->head);
 }
 
@@ -87,6 +91,7 @@ List * mergeLists(Element * list1, Element * list2){
     }
     
     while (list1 != NULL && list2 != NULL){
+        printf("in first while\n");
         Element * temp;
         //compare the first elements of each list
         
@@ -97,8 +102,13 @@ List * mergeLists(Element * list1, Element * list2){
         } else{ // compareNames must have returned 2, thus list2 is lower or they are the same
             temp = list2;
             temp->next = NULL;
+
             list2 = list2->next;
+if(list2 == NULL)
+printf("good\n");
         }
+
+
         if (result->head == NULL){ // if there are no elements in the result yet
             result->head = temp; // set the new element to be the first
             current = result->head; // and set the current pointer to it
@@ -107,24 +117,27 @@ List * mergeLists(Element * list1, Element * list2){
             current = temp; // and switch the current pointer
         }
     }
-    
     while (list1 != NULL){
+
         Element * temp;
         temp = list1;
+        list1 = list1->next;
         temp->next = NULL;
         current->next = temp;
         current = temp;
-        list1 = list1->next;
     }
     
     while(list2 != NULL){
+//char c = getchar();
         Element * temp;
         temp = list2;
+        list2 = list2->next;
         temp->next = NULL;
         current->next = temp;
         current = temp;
-        list1 = list2->next;
     }
+printf("returned list:\n");
+printList(result);
     return result;
 }
 
@@ -133,7 +146,7 @@ Element * bisectList(Element * head){
     List * temp = createList();
     temp->head = head;
     int len = getLength(temp);
-    if (len > 2)
+    if (len < 2)
         return NULL;
     else {
         if (len%2 == 0){ // even number of elements
@@ -141,6 +154,7 @@ Element * bisectList(Element * head){
         } else {
             len = len/2;
         }
+
         for (int i=0; i<len; i++){
             head = head->next;
         }
@@ -162,10 +176,10 @@ int compareNames(Element * first, Element * second){
 void printList(List * list){
     
     Element * temp = list->head;
+    while (temp != NULL){
     char *ln = ((Person*)(temp->elementPtr))->lastName;
     char *fn = ((Person*)(temp->elementPtr))->firstName;
     long num = ((Person*)(temp->elementPtr))->phoneNum;
-    while (temp != NULL){
         printf("%s, %s, %ld\n", ln, fn, num);
         temp = temp->next;
     }
