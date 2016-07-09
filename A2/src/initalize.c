96061a64989d9ef1939a2552e37adc73de00a022
jauthier <jauthier@mail.uoguelph.ca>
2016-07-08 21:41:54 -0400

    A2

--- A2/src/initalize.c
+++ A2/src/initalize.c
@@ -19,7 +19,7 @@ List * loadFile(char * fileName, List *list){
     
     fgets(buffer, 60, fp);
     
-    while (buffer != EOF){
+    while (buffer != NULL){
         
         lastName = strtok(buffer, ",");
         firstName = strtok(NULL, ",");
