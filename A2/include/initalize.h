6d67a852962ac98edb8eecd2bb88dc84fb0ffc4b
jauthier <jauthier@mail.uoguelph.ca>
2016-07-09 19:58:20 -0400

    A2

--- A2/include/initalize.h
+++ A2/include/initalize.h
@@ -1,4 +1,7 @@
-
+/*
+    Author: Jessica Authier
+    2016/07/08
+*/
 
 #ifndef INITALIZE_H
 #define INITALIZE_H
@@ -19,48 +22,58 @@ typedef struct Person{
 }Person;
 
 /*
-    Purpose:
-    Preconditions:
+    Purpose: to loop through the menu and action selection until the user wishes to exit
+    Preconditions: an initalized linked list and hash table
     Postconditions:
 */
 void menuLoop(List * list, Entry ** ht);
 
+/*
+    Purpose: to print the menu text
+    Preconditions: none
+    Postconditions: none
+*/
 void printMenu();
 
+/*
+    Purpose: to seach through a linked list and return the element that's elementPtr equals the void* given 
+    Preconditions: an initalized linked list which contains an Element matching the void* given as a parameter 
+    Postconditions: none
+*/
 Element * searchByValue(List * list, void * value);
 
 /*
-    Purpose:
-    Preconditions:
-    Postconditions:
+    Purpose: to return user input of type long
+    Preconditions: none
+    Postconditions: the message  given as a parameter is printed
 */
 long getInputLong(char * message);
 
 /*
-    Purpose:
-    Preconditions:
-    Postconditions:
+    Purpose: to return user input of type char* (string)
+    Preconditions: none
+    Postconditions: the message  given as a parameter is printed
 */
 char * getInputStr(char * message);
 
 /*
-    Purpose:
-    Preconditions:
-    Postconditions:
+    Purpose: to initalize a Person variable and set its vales to the values passed as parameters
+    Preconditions: none
+    Postconditions: a Person variable is created an is allocated the right amount of memory
 */
 Person * createPerson(char * ln, char * fn, long pn);
 
 /*
-    Purpose:
-    Preconditions:
-    Postconditions:
+    Purpose: to take the name of a file and load the contents of the file into a linked list
+    Preconditions: a properly formated file is given
+    Postconditions: each line of the file is used to create a person and is added to the list
 */
 List * loadFile(char * fileName, List *list, Entry ** ht);
 
 /*
-    Purpose:
-    Preconditions:
-    Postconditions:
+    Purpose: to print a linked list that has elementPtr's of type Person
+    Preconditions: an initalized none empty linked list
+    Postconditions: none
 */
 void printList(List * list);
 
--- A2/include/mergeSort.h
+++ A2/include/mergeSort.h
@@ -14,30 +14,30 @@
 #include "initalize.h"
 
 /*
-    Purpose:
-    Preconditions:
-    Postconditions:
+    Purpose: to sort a linked list
+    Preconditions: an initalized linked list exists
+    Postconditions: the given linked list is sorted alphabetically and returned
 */
 List * mergeSort(List * list);
 
 /*
-    Purpose:
-    Preconditions:
-    Postconditions:
+    Purpose: to take two lists and merge them into one list which is in alphabetical order
+    Preconditions: two initalized lists are given as parameters 
+    Postconditions: the new list is in alphabetical order
 */
 List * mergeLists(Element * list1, Element * list2);
 
 /*
-    Purpose:
-    Preconditions:
-    Postconditions:
+    Purpose: to find the middle of a linked list so the list can be spilt in two
+    Preconditions: an initalized linked list exists
+    Postconditions: the element in the middle of the list is returned
 */
 Element * bisectList(Element * head);
 
 /*
-    Purpose:
-    Preconditions:
-    Postconditions:
+    Purpose: to take two strings and detrmine which comes first in aphabetically
+    Preconditions: two initalized strings exist
+    Postconditions: 1 is returned if the first sting is first and 2 is returned if the second string comes first
 */
 int compareNames(Element * first, Element * second);
 
