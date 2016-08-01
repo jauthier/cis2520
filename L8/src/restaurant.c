65220ea4f00740d1b865d492418c3df54f716a17
jauthier <jauthier@mail.uoguelph.ca>
2016-08-01 19:47:11 -0400

    l8

--- L8/src/restaurant.c
+++ L8/src/restaurant.c
@@ -17,7 +17,7 @@ Restaurant * createRestaurant(char * name, char * type, int rating) {
     newR->type = malloc(sizeof(char)*strlen(type));
     strcpy(newR->type, type);
     
-    &newR->rating = malloc(sizeof(int));
+    &(newR->rating) = malloc(sizeof(int));
     newR->rating = rating;
     
     return newR;
