7cb504a7fedf4da49c51a16bcef46a776d4f4dd3
jauthier <jauthier@mail.uoguelph.ca>
2016-08-02 00:37:22 -0400

    addingL8

--- L8/src/main.c
+++ L8/src/main.c
@@ -11,17 +11,46 @@ int main(int argc, char *argv[]){
         exit(0);
     }
     
+    Tree * nameTree = createBinTree(&compareName, &destroyRestaurant);
+    Tree * ratingTree = createBinTree(&compareRating, &destroyRestaurant);
     List * list = createList();
     list = parseFile(fileName, list);
+    
+    
     
     Element * hold = list->head;
     while (hold != NULL){
         
-        
+        addToTree(nameTree, hold->elementPtr);
+        addToTree(ratingTree, hold->elementPtr);
         
         hold = hold->next;
     }
     
+    printf("Sort By Name\n------------\n");
+    printInOrder(nameTree, &printRestaurant); // print in order
+    printf("\n");
+    printTree(nameTree, 0); // print as a tree
+    printf("\n");
+    
+    printf("Sort By Rating\n---------------\n");
+    printInOrder(ratingTree, &printRestaurant); //print in order
+    printf("\n");
+    printTree(ratingTree, 0); // print as a tree
+    printf("\n");
+    
+    hold = list->head;
+    while (hold != NULL){
+        
+        removeFromTree(nameTree, hold->elementPtr);
+        removeFromTree(ratingTree, hold->elementPtr);
+        
+        hold = hold->next;
+    }
+    
+    destroyList(list);
+    destroyBinTree(nameTree);
+    destroyBinTree(ratingTree);
     
     return 0;
 }
